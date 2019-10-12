74a112b2efa926f3dbb5e92eb94b130351db083e
Gavin <ooigavin@gmail.com>
2019-10-12 19:41:35 +0800

    Add cli args into commands

--- pomo_cli.py
+++ pomo_cli.py
@@ -1,37 +1,38 @@
 import argparse
 from threading import Timer
-import os
 import subprocess
+import sys
+
+
+parser = argparse.ArgumentParser(description='Pomodoro cli in python')
+
+parser.add_argument('work', metavar='work_time', type=int, help='Working time in minutes')
+parser.add_argument('rest', metavar='rest_time', type=int, help='Resting time in minutes')
+parser.add_argument('-l', metavar='limit', type=int, help='Limit to pomodoros')
+
+args = parser.parse_args()
 
 pomo_done = 0
-rest_time = 2
-work_time = 5
-limit = 1000
+rest_time = args.rest * 60
+work_time = args.work * 60
+limit = args.l if args.l else 1000
 
-
-def times_up(done, msg):
-  print(f'Times up time for break {done} pomos done')
-  subprocess.call("mpg123 -q /home/gavin/MRobot/Sounds/bell.mp3", shell=True)
-  os.system(f'spd-say "{msg}"')
+def times_up(msg, sound):
+  print(msg)
+  subprocess.call(f"mpg123 -q /home/gavin/MRobot/Sounds/{sound}.mp3", shell=True)
 
 
 def do_pomo(pomo_done, work_time, rest_time):
   work_msg = 'Work work'
-  rest_msg = 'Time for break mofo'
-  work = Timer(work_time, times_up, args=(pomo_done,rest_msg))
+  rest_msg = f'Time for break \n{pomo_done} pomos done'
+  work = Timer(work_time, times_up, args=(rest_msg, 'bell'))
   work.start()
   work.join()
-  rest = Timer(rest_time, times_up, args=(pomo_done,work_msg))
+  rest = Timer(rest_time, times_up, args=(work_msg, 'fog'))
   rest.start()
   rest.join()
 
 
 while pomo_done < limit:
   pomo_done+=1
-  do_pomo(pomo_done, 10, 5)
-
-# add args
-
-# show timer
-
-# play sound
+  do_pomo(pomo_done, work_time, rest_time)
